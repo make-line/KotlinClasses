d999d2bec3d0a291e46e95e82a28f5d2d82825c0
make-line <enariiana@gmail.com>
2022-04-22 18:01:29 +0300

    8+

--- src/main/kotlin/ThreadPool.kt
+++ src/main/kotlin/ThreadPool.kt
@@ -4,12 +4,11 @@ import java.util.concurrent.LinkedBlockingQueue
 class ThreadPool() : Executor {
     private var threads = mutableListOf<WorkerThread>()
     private var tasks = LinkedBlockingQueue<Runnable>()
-    private val obj = Object()
 
     constructor(count: Int) : this() {
         if (count <= 0) throw IllegalArgumentException("недопустимое клоичество потоков")
-        for (i in 1..count) {
-            val thread = WorkerThread(tasks, obj)
+        repeat(count){
+            val thread = WorkerThread(tasks)
             thread.start()
             threads.add(thread)
         }
@@ -17,18 +16,42 @@ class ThreadPool() : Executor {
 
 
     override fun execute(command: Runnable) {
-        synchronized(obj) {
+        synchronized(tasks) {
             tasks.add(command)
-            obj.notify()
+            (tasks as Object).notify()
         }
 
 
     }
 
     fun shutdown() {
-        threads.forEach{
-            it.stopState = true
-            it.interrupt()
+        threads.forEach {
+            it.setStopState(true)
+            it.interrupt() }
+    }
+
+    private inner class WorkerThread(private val tasks: LinkedBlockingQueue<Runnable>) : Thread() {
+        @Volatile
+        private var stopState= false
+        fun setStopState (bool: Boolean){ this.stopState=bool}
+        override fun run() {
+            while (true) {
+                var task: Runnable? = null
+                if (stopState) break
+                synchronized(tasks) {
+                    if (!tasks.isEmpty()) {
+                        task = tasks.remove()
+                    } else {
+                        try {
+                            (tasks as Object).wait()
+                        } catch (e: InterruptedException) {
+                            currentThread().interrupt()
+                        }
+                    }
+
+                }
+                task?.run()
+            }
         }
     }
 
